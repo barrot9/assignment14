06f03b63ef1abee602b3edbbf39bea4a2bd8fcd9
RenanaSol <renanasolomon2000@gmail.com>
2024-08-05 12:08:32 +0300

    Add file that include all the assistance functions

---
+++ utils.c
@@ -0,0 +1,23 @@
+#include "utils.h"
+#include <ctype.h>
+#include <string.h>
+
+/* Implementation of trimWhitespace function */
+char *trimWhitespace(char *str) {
+    char *end;
+
+    /* Trim leading space */
+    while (isspace((unsigned char)*str)) str++;
+
+    if (*str == 0)  /* All spaces? */
+        return str;
+
+    /* Trim trailing space */
+    end = str + strlen(str) - 1;
+    while (end > str && isspace((unsigned char)*end)) end--;
+
+    /* Write new null terminator */
+    end[1] = '\0';
+
+    return str;
+}
---
+++ utils.h
@@ -0,0 +1,12 @@
+#ifndef UTILS_H
+#define UTILS_H
+
+/**
+ * Trims leading and trailing whitespace from a string.
+ * 
+ * @param str The string to trim.
+ * @return A pointer to the trimmed string.
+ */
+char *trimWhitespace(char *str);
+
+#endif /* UTILS_H */
